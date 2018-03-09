996189b3e76023110b4395a05c2d1a6fd90e0642
Mình đẹp trai chết đi được <root@lulu>
2018-03-09 11:06:27 -0500

    Router Get demo

---
+++ NodeUdemy/main.js
@@ -0,0 +1,45 @@
+const httpServer = require('http');
+const url = require('url');
+
+let routes = {
+    'GET': {
+        '/': (req, res)=> {
+            res.writeHead(200, {'Content-type': 'text/html'});
+            // res.write(req.cookie);
+            res.end('<h2>Hello Router</h2>');
+        },
+        '/about': (req, res)=> {
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.end('This is about page');
+        },
+        '/api/getinfo': (req, res)=> {
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.end(JSON.stringify(req.queryParam));
+        },
+    },
+    'POST': {
+
+    },
+    'NA': (req, res)=> {
+        res.writeHead(404);
+        res.end('Content not found');
+    },
+};
+
+function router(request, respone) {
+    let baseURI = url.parse(request.url, true);
+
+    let resolveRoute = routes[request.method][baseURI.pathname];
+
+    if (resolveRoute != undefined) {
+        request.queryParam = baseURI.query;
+        resolveRoute(request, respone);
+    }
+    else {
+        routes['NA'](request, respone);
+    }
+}
+
+httpServer.createServer(router).listen(8080, ()=> {
+   console.log('Server listen on port 8080');
+});
